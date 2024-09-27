9e4879adf17fe5c5b7a8d1fcaaf32ff9afb5368c
Josue_pc <davidchitay699@gmail.com>
2024-09-27 00:46:56 -0600

    Primera version proyecto

---
+++ src/Conexion.java
@@ -0,0 +1,69 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class Conexion {
+    String db = "dbproyecto_algoritmos";
+    String url = "jdbc:mysql://localhost:3306/" +db;
+    String user = "root";
+    String password = "";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection cx;
+    
+    
+
+    public Conexion() {
+    }
+
+    public Conexion(String dbproyecto_algoritmos) {
+        this.db = db;
+    }
+
+    public Connection conectar(){
+        try {
+            Class.forName(driver);
+            cx=DriverManager.getConnection(url, user, password);
+            System.out.println("se conecto a base de datos" + db);
+                    } catch (ClassNotFoundException | SQLException ex) {
+                        System.out.println("No se conecto a base de datos" + db);
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cx;
+    }
+    
+    public void desconectar(){
+        try {
+            cx.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args){
+        Conexion conn = new Conexion();
+        conn.conectar();
+    }
+
+    Connection getConnection() {
+        try {
+            // Cargar el controlador JDBC
+            Class.forName(driver);
+            // Establecer la conexión con la base de datos
+            cx = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa a la base de datos: " + db);
+        } catch (ClassNotFoundException | SQLException ex) {
+            // Manejo de errores si ocurre un problema al conectar
+            System.out.println("Error al conectar a la base de datos: " + ex.getMessage());
+            ex.printStackTrace(); // Imprime el error en la consola
+        }
+        return cx;
+    }
+
+   
+}
---
+++ src/login.java
@@ -0,0 +1,122 @@
+
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.sql.SQLException;
+import javax.swing.ImageIcon;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+
+
+public class login extends javax.swing.JFrame {
+
+Conexion cx;
+    public login() {
+        initComponents();
+        this.setTitle("LOGIN");
+        this.setSize(400, 400);
+        Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/img/iniciar-sesion2.png"));
+        lblLogo.setIcon(new ImageIcon(img.getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(),Image.SCALE_SMOOTH)));
+        this.setIconImage(img);
+        this.setLocationRelativeTo(null);
+        cx = new Conexion("dbproyecto_algoritmos");
+        cx.conectar();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        txtUsuario = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        btnIniciar = new javax.swing.JButton();
+        btnCancelar = new javax.swing.JButton();
+        lblLogo = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JPasswordField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("Usuario");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 190, -1, -1));
+        getContentPane().add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 180, 144, -1));
+
+        jLabel2.setText("Password");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, -1, -1));
+
+        btnIniciar.setText("Iniciar");
+        btnIniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIniciarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnIniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 280, 114, 42));
+
+        btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 280, 135, 42));
+
+        lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/iniciar-sesion2.png"))); // NOI18N
+        lblLogo.setText("jLabel3");
+        getContentPane().add(lblLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 20, 140, 130));
+        getContentPane().add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 140, -1));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
+    try {
+        String user=txtUsuario.getText();
+        String password=String.valueOf(txtPassword.getPassword());
+        String query = "SELECT * FROM usuario WHERE user='"+ user +"' AND password='"+ password +"'";
+        Statement st = cx.conectar().createStatement();
+        ResultSet rs = st.executeQuery(query);
+        if(rs.next()){
+            JOptionPane.showMessageDialog(this, "EL USUARIO EXISTE EN LA BASE DE DATOS");
+            dispose();
+            menu ir = new menu();
+            ir.setVisible(true);
+        }else{
+            JOptionPane.showMessageDialog(this, "EL USUARIO NO EXISTE EN LA BASE DE DATOS");
+        }
+        
+    } catch (SQLException ex) {
+        Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        
+    }//GEN-LAST:event_btnIniciarActionPerformed
+
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new login().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnIniciar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lblLogo;
+    private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JTextField txtUsuario;
+    // End of variables declaration//GEN-END:variables
+}
